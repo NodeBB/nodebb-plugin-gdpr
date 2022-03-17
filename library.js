1bcd5050af8fc70d76a0a2374f49bf7f5084f893
Julian Lam <julian@nodebb.org>
2022-03-17 09:50:21 -0400

    refactor: rewrite some methods to async..await

--- library.js
+++ library.js
@@ -7,17 +7,11 @@ var meta = require.main.require('./src/meta');
 var user = require.main.require('./src/user');
 var pagination = require.main.require('./src/pagination');
 
-var winston = require.main.require('winston');
-
 var controllers = require('./lib/controllers');
 
 var plugin = {};
 
-plugin.init = function (params, callback) {
-	var router = params.router;
-	var hostMiddleware = params.middleware;
-	var hostControllers = params.controllers;
-
+plugin.init = ({ router, middleware: hostMiddleware }) =>  {
 	var SocketPlugins = require.main.require('./src/socket.io/plugins');
 
 	SocketPlugins.gdpr = {};
@@ -27,25 +21,6 @@ plugin.init = function (params, callback) {
 
 	router.get('/admin/plugins/gdpr', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
 	router.get('/api/admin/plugins/gdpr', controllers.renderAdminPage);
-
-	plugin.syncSettings(callback);
-};
-
-plugin.syncSettings = function (callback) {
-	meta.settings.get('gdpr', function (err, settings) {
-		if (err) {
-			return callback(err);
-		}
-
-		plugin.settings = Object.assign((plugin.settings || {}), settings);
-		callback();
-	});
-};
-
-plugin.onSettingsChange = function (data) {
-	if (data.plugin === 'gdpr') {
-		plugin.settings = Object.assign((plugin.settings || {}), data.settings);
-	}
 };
 
 plugin.getUsers = function (page, callback) {
@@ -102,20 +77,16 @@ plugin.addAdminNavigation = function (header, callback) {
 	callback(null, header);
 };
 
-plugin.appendConfig = function (config, callback) {
-	db.getObjectField('user:' + config.uid, 'gdpr_consent', function (err, consented) {
-		if (err) {
-			winston.error('[plugin/gdpr] Could not append info to uid  ' + config.uid + ': ' + err.message);
-			return setImmediate(callback, null, config);
-		}
+plugin.appendConfig = async (config) => {
+	const consented = await db.getObjectField('user:' + config.uid, 'gdpr_consent');
+	const { require_consent } = await meta.settings.get('gdpr');
 
-		config.gdpr = {
-			require: !!(plugin.settings.require_consent === 'on'),
-			given: !!parseInt(consented, 10),
-		};
+	config.gdpr = {
+		require: require_consent === 'on',
+		given: !!parseInt(consented, 10),
+	};
 
-		callback(null, config);
-	});
+	return config;
 };
 
 module.exports = plugin;
